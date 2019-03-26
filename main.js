03fa78a9d2e1a766eeabf96b9966d06194b1f469
Wyatt Wortkoetter <wwortkoetter@gmail.com>
2019-03-26 11:32:03 -0400

    <List>.append() --> <List>.push()

--- main.js
+++ main.js
@@ -16,19 +16,19 @@ window.onload = function () {
   // var ll1 = new mapboxgl.LngLat(-84.516,39.131);
   // var ll2 = new mapboxgl.LngLat(-82.991111,40.009444);
 
-  // var marker1 = new mapboxgl.Marker()
-  // .setLngLat([-84.516,39.131])
-  // .addTo(map);
-  // var marker2 = new mapboxgl.Marker()
-  // .setLngLat([-82.991111,40.009444])
-  // .addTo(map);
-  // var marker3 = new mapboxgl.Marker()
-  // .setLngLat([-84.401,33.755])
-  // .addTo(map);
+  var marker1 = new mapboxgl.Marker()
+  .setLngLat([-84.516,39.131])
+  .addTo(map);
+  var marker2 = new mapboxgl.Marker()
+  .setLngLat([-82.991111,40.009444])
+  .addTo(map);
+  var marker3 = new mapboxgl.Marker()
+  .setLngLat([-84.401,33.755])
+  .addTo(map);
 
   var markerList = {};
   for (var i = 0; i < teams['features'].length; i++) {
-    markerList.append(new mapboxgl.Marker().setLngLat(teams['features'][i]['geometry']['coordinates']));
+    markerList.push(new mapboxgl.Marker().setLngLat(teams['features'][i]['geometry']['coordinates']));
     console.log("lööp");
   };
   console.log(markerList)
